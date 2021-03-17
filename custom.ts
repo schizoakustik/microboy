7df361da3b32f7c09237bfe0de15089d68457a77
Linus Rundberg Streuli <schizoakustik@schizoakustik.se>
2021-03-17 23:27:00 +0100

    Initial files for MakeCode project

---
+++ custom.ts
@@ -0,0 +1,16 @@
+/**
+ * Micro:boy
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace microboy {
+    /**
+     * TODO: describe your function here
+     * @param n describe parameter here, eg: 5
+     * @param s describe parameter here, eg: "Hello"
+     * @param e describe parameter here
+     */
+    //% block
+    export function foo(n: number, s: string): void {
+        // Add code here
+    }
+}
